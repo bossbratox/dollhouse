1c11079ea7b25e870cac07f9a24332c13060cda3
Wattsy <wattsyart@protonmail.com>
2022-01-20 14:32:22 -0500

    flip to promo script

---
+++ test/dollhousePromo.js
@@ -0,0 +1,19 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("DollhousePromo", function () {
+
+  var DollhousePromo;
+
+  beforeEach(async () => {
+    [owner, stranger] = await ethers.getSigners();
+    const DollhousePromoFactory = await ethers.getContractFactory("DollhousePromo");
+    DollhousePromo = await DollhousePromoFactory.deploy(owner.address, 1000, 100);
+    await DollhousePromo.deployed();
+  });
+
+  it("should deploy", async () => {
+    expect(await DollhousePromo.name()).to.equal("Dollhouse Promo");
+    expect(await DollhousePromo.symbol()).to.equal("DOLLPROMO");
+  });
+});
